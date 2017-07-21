d95c5e3dd28001419403793a6e9514b28ecb13b2
Alec Wilson <alecw1992@gmail.com>
2017-07-21 13:05:37 +0100

    Model added for enquiries.

---
+++ src/AppBundle/Entity/Enquiry.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Model for the enquiries table.
+ */
+class Enquiry
+{
+    private $id;
+    private $name;
+    private $email;
+    private $content;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content)
+    {
+        $this->content = $content;
+        return $this;
+    }
+}
